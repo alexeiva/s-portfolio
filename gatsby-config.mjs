d786cac5fd885d598aa2dcf72a6d990093eb1c0d
Alexei Vanyashin <a+mygit@cyreal.org>
2023-07-21 00:00:55 +0300

    fixed homepage to projects only

---
+++ gatsby-config.mjs
@@ -0,0 +1,23 @@
+// gatsby-config.mjs
+const config = {
+  siteMetadata: {
+    // Used for the title template on pages other than the index site
+    siteTitle: `Alexei Vanyashin — Portrolio`,
+    // Default title of the page
+    siteTitleAlt: `Personal website of Alexei Vanyashin`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Alexei Vanyashin Typography`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `http://110design.ru`,
+    // Used for SEO
+    siteDescription: `Alexei Vanyashin is a type designer, and educator.`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Set the default "lang" attribute on "html" element
+    siteLanguage: `en`,
+    // Twitter Handle
+    author: `@avanyashin`,
+  },
+};
+
+export default config;
