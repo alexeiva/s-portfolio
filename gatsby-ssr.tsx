736ef09c86460972cb1be58ac40b201bb34c8abe
Alexei Vanyashin <a+mygit@cyreal.org>
2024-03-08 01:30:47 +0600

    fixed font

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -5,11 +5,19 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents }) =
   setHeadComponents([
     <link
       rel="preload"
-      href="static/fonts/Recursive-VariableFont_CASL,CRSV,MONO,slnt,wght.woff2"
+      href="static/fonts/Rubik-VariableFont_wght.woff2"
       as="font"
       type="font/woff2"
       crossOrigin="anonymous"
-      key="recursiveFont"
+      key="rubikFont"
+    />,
+    <link
+      rel="preload"
+      href="static/fonts/Rubik-Italic-VariableFont_wght.woff2"
+      as="font"
+      type="font/woff2"
+      crossOrigin="anonymous"
+      key="rubikFont"
     />,
   ])
 }
